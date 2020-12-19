bcd08f791fc2f5785732a5bcca150941e31de6b0
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2020-12-19 18:01:30 +0000

    Create the flask app

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from flask import Flask, render_template
+if os.path.exists("env.py"):
+    import env
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    """Main Page with Start Button"""
+    return "Hello world ... again"
+    # return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True)
