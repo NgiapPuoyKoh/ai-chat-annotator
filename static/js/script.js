c9d21247ecdbffbd51dcc66a92b1df48c7053a0e
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2021-04-24 00:53:54 +0000

    Fix Page reload to store input text with poling setPageReload()

--- static/js/script.js
+++ static/js/script.js
@@ -25,3 +25,31 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Modal.init(elems);
   });
 
+function setPageReload() {
+    let msgtxt = document.getElementById("msgtxt")
+
+    // Get the value from the localStorage if there is a value
+    let storeText = localStorage.getItem("msgtxt");
+    if (storeText !== "") {
+        msgtxt.textContent = storeText;
+    }
+    let timer = setTimeout(function () {
+        // Update the localStorage anytime the is a pull
+        localStorage.setItem("msgtxt", msgtxt.value);
+        location.reload();
+    }, 10000);
+
+    // Set the focus
+    msgtxt.focus();
+
+    // Move the cursor to the end of the input text
+    const len = msgtxt.value.length;
+    msgtxt.setSelectionRange(len, len);
+
+    // Get the form element and add a event handler to capture the submit
+    // event and clear the input text after submitting the message.
+    const chatForm = document.getElementById('chat');
+    chatForm.addEventListener("submit", () => {
+        localStorage.setItem("msgtxt", "");
+    });
+}
